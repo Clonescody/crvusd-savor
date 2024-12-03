0eea806e0373aa8b8635be7283b769b42757cc15
clonescody <clonescody@protonmail.com>
2024-12-03 02:47:51 +0100

    try umami setting for vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   define: {
-    __APP_ENV__: process.env.VITE_API_URL,
+    "process.env": {},
   },
   plugins: [react()],
   resolve: {
